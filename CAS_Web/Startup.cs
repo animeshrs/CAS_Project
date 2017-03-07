fc19d9c2016a36d47259f14461c1c39eb97217ce
ramesh <rameshp89@yahoo.co.in>
2017-03-07 23:14:41 +0000

    CAS Web Project Checked-in

---
+++ CAS_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CAS_Web.Startup))]
+namespace CAS_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
